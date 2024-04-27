d09989679493121d67e870f5ffd0be0b55ca84e3
nra291 <42358855+nra291@users.noreply.github.com>
2024-04-27 11:44:45 +0530

    Description for change

---
+++ git.sh
@@ -0,0 +1,17 @@
+
+# ---------------
+# Every Time
+# ---------------
+git add .
+git commit -m "Description for change"
+git push -u origin master
+
+
+# ---------------
+# First Time
+# ---------------
+# git init
+# git add .
+# git commit -m "Description for change"
+# git push -u origin master
+# git remote add origin https://github.com/{YOUR_USERNAME}/{YOUR_REPO_NAME}.git
