29f7fe8abb5f996dae94fcd4fcbd2d0170ba3be3
nra291 <42358855+nra291@users.noreply.github.com>
2024-05-02 12:11:39 +0530

    Description for change

--- code/dash_app_v1.0.py
+++ code/dash_app_v1.0.py
@@ -12,20 +12,11 @@ import config as c
 
 
 d1, d2 = h.prepare_data(c.rel_data_folder)
-h.set_config() # Should be the first StrealLit Command in the app
-
-# top_row_col_1, top_row_col_2 = h.l.st.columns(1)
-# top_row_col_1 = h.l.st.columns()
 
 
-bottom_col_1, bottom_col_2= h.l.st.columns(2)
-
-with bottom_col_1:
-    h.st_plot_overall(d2)
-with bottom_col_2:
-    h.plot_pnl(d2)
-
+h.set_config()
 h.st_sidebar(d1)
+h.st_total_investment(d2)
 
-#
-    
+
+col = h.l.st.columns((1.5, 4.5, 2), gap='medium')
--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 
+
 import helper_functions as h
 import config as c
 
@@ -12,12 +13,20 @@ import config as c
 
 
 d1, d2 = h.prepare_data(c.rel_data_folder)
+h.set_config() # Should be the first StrealLit Command in the app
+
+# top_row_col_1, top_row_col_2 = h.l.st.columns(1)
+# top_row_col_1 = h.l.st.columns()
 
 
-h.set_config()
+bottom_col_1, bottom_col_2= h.l.st.columns(2)
+
+with bottom_col_1:
+    h.st_plot_overall(d2)
+with bottom_col_2:
+    h.plot_pnl(d2)
+
 h.st_sidebar(d1)
-h.st_total_investment(d2)
 
-
-col = h.l.st.columns((1.5, 4.5, 2), gap='medium')
-
+#
+    
