cf18cf675e5b6c92774322898a819ea191924662
nra291 <42358855+nra291@users.noreply.github.com>
2024-05-10 00:40:20 +0530

    Description for change

---
+++ code/test.py
@@ -0,0 +1,20 @@
+
+import helper_functions as h
+import config as c
+import time
+import os
+import streamlit as st
+
+
+d1, d2 = h.prepare_data(c.rel_data_folder)
+
+import plotly.express as px
+
+# z = [[.1, .3, .5, .7, .9],
+#      [1, .8, .6, .4, .2],
+#      [.2, 0, .5, .7, .9],
+#      [.9, .8, .4, .2, 0],
+#      [.3, .4, .5, .7, 1]]
+
+fig = px.imshow(d1, text_auto=True)
+fig.show()
