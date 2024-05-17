c5af88c904000de310cb4c3c6c29853063bd7009
nra291 <42358855+nra291@users.noreply.github.com>
2024-05-17 18:08:37 +0530

    Description for change

--- code/helper_functions.py
+++ code/helper_functions.py
@@ -437,9 +437,9 @@ def plot_invested_share(df,col_val, layout_col):
 
 def plot_profit_share(df,col_val, layout_col):
     if col_val == c.mf_key:
-        plot_donut(df, 'Profit by Fund-Folio', col_val, c.percent_return, layout_col)
+        plot_donut(df, 'Profit by Fund-Folio', col_val, c.returns, layout_col)
     elif col_val == c.amc:
-        plot_donut(df, 'Profit by AMC', col_val, c.percent_return, layout_col)
+        plot_donut(df, 'Profit by AMC', col_val, c.returns, layout_col)
 
 def make_heatmap(input_df, input_x, input_y, input_color, input_color_theme):
 
