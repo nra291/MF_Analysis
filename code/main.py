f7e4c18c2cd9d1b0fef56c35e82717fd34b6c154
nra291 <42358855+nra291@users.noreply.github.com>
2024-05-28 20:38:15 +0530

    Description for change

--- code/main.py
+++ code/main.py
@@ -50,7 +50,7 @@ h.plot_stacked_bar(data_by_key, c.mf_key)
 
 
 with h.l.st.expander("Funds Comparison"):
-    h.st_comparison(d1)
+    h.st_comparison(d1) #
 
 
 # h.make_heatmap(d1, c.as_of_date, c.percent_return,c.mf_key, 'blues')
